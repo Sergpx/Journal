76db41c8587bc64822e18c0a89207178c641b1ea
Sergp <tinker.step3267>
2023-06-09 21:17:09 +0300

    ha

---
+++ myStr.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "MyString.h"
+using namespace std;
+
+int main()
+{
+    MyString test = "abcdg";
+    MyString test2 = "abcde";
+    MyString test3 = test + test2;
+    test3.show();
+    
+    
+
+}
